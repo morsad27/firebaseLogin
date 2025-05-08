3c8bee097adce87d46d8699e02ab2dcd329fabfc
morsad27 <almorsada.tagal@gmail.com>
2025-05-08 14:14:24 +0800

    login/signup working

---
+++ firebase.client.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGrXE9vSfEZQCTU0JUF53Ru7pVBMgL6AI",
+  authDomain: "loginfirebase-90297.firebaseapp.com",
+  projectId: "loginfirebase-90297",
+  storageBucket: "loginfirebase-90297.firebasestorage.app",
+  messagingSenderId: "872460555466",
+  appId: "1:872460555466:web:c996888439e78f05bbd033",
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
